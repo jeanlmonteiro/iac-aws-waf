7f21bb867507eb89c893d92d4f5389927403bf4f
Jean Louis <jean@macetado.com>
2025-01-26 09:59:05 -0300

    ajustes

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
